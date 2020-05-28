f0ed85c02d0a2d279fbb06afcc4a835ba6373fa2
Khanh Do Quoc <khanh451754@gmail.com>
2020-05-28 09:33:30 +0700

    WIP

--- src/App.js
+++ src/App.js
@@ -7,26 +7,52 @@ import { data } from "./data";
 function App() {
   const [board, setBoard] = useState(data);
   // const [tasks, setTasks] = useState([]);
-  const boardData = localStorage.getItem("boardData");
+  const workboard = JSON.parse(localStorage.getItem("workboard"));
+
+  function addTaskToLocalStorage(board, card, laneIndex) {
+    const updatedBoard = board;
+    updatedBoard.lanes[laneIndex].cards.push(card);
+    setBoard(updatedBoard);
+    localStorage.setItem("workboard", JSON.stringify(updatedBoard));
+  }
+
+  function deleteTaskToLocalStorage(board, cardIndex, laneIndex) {
+    const updatedBoard = board;
+    updatedBoard.lanes[laneIndex].cards.splice(cardIndex, 1);
+    setBoard(updatedBoard);
+    localStorage.setItem("workboard", JSON.stringify(updatedBoard));
+  }
 
   function onCardAdd(card, laneId) {
-    console.log(card, laneId, board);
-    // const index = findIndex(board, ["lanes.id", laneId]);
-    console.log(index);
+    const boardLanes = get(board, "lanes", []);
+    const laneIndex = findIndex(boardLanes, ["id", laneId]);
+    addTaskToLocalStorage(board, card, laneIndex);
+  }
+
+  function onCardDelete(cardId, laneId) {
+    console.log(cardId, laneId, board);
+    const boardLanes = get(board, "lanes", []);
+    const laneIndex = findIndex(boardLanes, ["id", laneId]);
+
+    const laneCards = get(boardLanes[laneIndex], "cards", []);
+    const cardIndex = findIndex(laneCards, ["id", cardId]);
+    deleteTaskToLocalStorage(board, cardIndex, laneIndex);
   }
 
   useEffect(() => {
-    // if (boardData) {
-    //   setBoard(boardData);
-    // }
-  }, [boardData]);
-
-  // localStorage.setItem('boardData', data)
-  // console.log(board);
+    if (workboard) {
+      setBoard(workboard);
+    }
+  }, []);
 
   return (
     <div className="App">
-      <Board data={board} editable onCardAdd={onCardAdd} />
+      <Board
+        data={board}
+        editable
+        onCardAdd={onCardAdd}
+        onCardDelete={onCardDelete}
+      />
     </div>
   );
 }
